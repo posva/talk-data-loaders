f57332473e185950d97541061863fd2fc9173eab
Eduardo San Martin Morote <posva13@gmail.com>
2024-10-09 15:43:56 +0200

    ok

--- demo-start/src/main.ts
+++ demo-start/src/main.ts
@@ -3,13 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import { router } from './router'
-import { DataLoaderPlugin } from 'vue-router/auto'
-import { QueryPlugin } from '@pinia/colada'
+import { DataLoaderPlugin } from 'unplugin-vue-router/data-loaders'
+import { PiniaColada } from '@pinia/colada'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(QueryPlugin, {
+app.use(PiniaColada, {
   // ...
 })
 app.use(DataLoaderPlugin, { router })
